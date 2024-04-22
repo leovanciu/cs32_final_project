77bc19e207504e7d199e78f5d9b524b364578415
leovanciu <leo.vanciu@gmail.com>
2024-04-22 04:38:59 -0400

    Update scripts

--- R/vector.R
+++ R/vector.R
@@ -3,16 +3,25 @@ setwd("/Users/ancavanciupopescu/Desktop/Classes/CS 32/Final project/Data/")
 data <- read.csv("loop_data.csv")[,2]
 
 # Generic loop to compute geometric mean
-loop <- function(n) {
-  for (i in 1:n) {
-   i = i + 1
+geometric_mean_loop <- function(data) {
+  if (any(data <= 0)) return(NA)
+  log_sum <- 0
+  n <- length(data)
+  for (num in data) {
+    log_sum <- log_sum + log(num)
   }
-  return(i)
+  return(exp(log_sum / n))
+}
+
+# Vectorized computation of geometric mean
+geometric_mean_vectorized <- function(data) {
+  if (any(data <= 0)) return(NA)
+  return(exp(mean(log(data))))
 }
 
 # Measure execution time and memory
 results <- bench::mark(
-  loop = { loop_result <- loop(n) },
+  loop = { loop_result <- geometric_mean_loop(data) },
   vectorized = { vectorized_result <- geometric_mean_vectorized(data) },
   iterations = 10,
   check = FALSE
